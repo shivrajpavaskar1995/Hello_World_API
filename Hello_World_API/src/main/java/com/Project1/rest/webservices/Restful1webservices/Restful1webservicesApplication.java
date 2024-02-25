4fb7f7af19a484950f9978a1be610773beb4aecc
shivrajpavaskar1995 <143311102+shivrajpavaskar1995@users.noreply.github.com>
2024-02-25 19:33:13 +0530

    Add files via upload

---
+++ Hello_World_API/src/main/java/com/Project1/rest/webservices/Restful1webservices/Restful1webservicesApplication.java
@@ -0,0 +1,13 @@
+package com.Project1.rest.webservices.Restful1webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Restful1webservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Restful1webservicesApplication.class, args);
+	}
+
+}
---
+++ Hello_World_API/src/test/java/com/Project1/rest/webservices/Restful1webservices/Restful1webservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Project1.rest.webservices.Restful1webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Restful1webservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
